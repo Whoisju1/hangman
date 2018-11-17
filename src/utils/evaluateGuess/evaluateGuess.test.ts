ad4be9f57fcc7e543cead69f1b3342fb870c0c1c
Juan John-Charles <whoisju1@gmail.com>
2018-11-16 20:01:46 -0500

    refactor and retest the evaluate guess function

--- src/utils/evaluateGuess/evaluateGuess.test.ts
+++ src/utils/evaluateGuess/evaluateGuess.test.ts
@@ -3,21 +3,18 @@ import { evaluateGuess } from './evaluateGuess';
 describe('evalGuess function', () => {
   it('should return an array of objects that contain' +
   ' the letter and it\'s index where it is found in the answer', () => {
-    const expected = [
-      {
-        index: 0,
-        key: 'e',
-      },
-      {
-        index: 7,
-        key: 'e',
-      },
-    ];
-
+    const expected = {
+      indexes: [0, 7],
+      key: 'e',
+    };
     expect(evaluateGuess({ answer: 'evaluate', letterGuessed: 'e' })).toEqual(expected);
   });
 
   it('should return an empty array if the guessed letter does not match.', () => {
-    expect(evaluateGuess({ answer: 'answer', letterGuessed: 'z' })).toEqual([]);
+    expect(evaluateGuess({ answer: 'answer', letterGuessed: 'z' }))
+      .toEqual({
+        indexes: [],
+        key: 'z',
+      });
   });
 });
--- src/utils/evaluateGuess/evaluateGuess.ts
+++ src/utils/evaluateGuess/evaluateGuess.ts
@@ -1,6 +1,8 @@
+import { isNumber } from 'util';
+
 export interface IGuessEvalResult {
   key: string;
-  index: number;
+  indexes: number[];
 }
 
 interface IParams {
@@ -12,18 +14,22 @@ interface IParams {
  * @param {Object} guessAndAnswer - An object that bears the target word and the letter guessed
  * @param {string} guessAndAnswer.answer - The word that the user is trying to get
  * @param {string} guessAndAnswer.letterGuessed - The letter that the user guessed
- * @returns {Array<{ key: string, index: number}>} - An array of the letter pressed with its indexes in the target word
+ * @returns {Array<{ key: string, indexes: number[]}>} - An array of the letter pressed with its
+ *                                                     indexes in the target word
  */
-export const evaluateGuess = ({ answer, letterGuessed }: IParams): IGuessEvalResult[] => (
-  answer.split('')
-    .reduce((arr: IGuessEvalResult[], letter, index) => {
-      if (letterGuessed === letter) {
-        const obj = {
-          index,
-          key: letterGuessed,
-        };
-        arr.push(obj);
-      }
-      return arr;
-    }, [])
-);
+export const evaluateGuess = ({ answer, letterGuessed: key }: IParams): IGuessEvalResult => {
+  const indexes: number[] = answer.split('')
+    .reduce((previousIndex, letter, index) => {
+      // if the letter id found in the word push its index into the indexes array
+      // if the letter does not exist in the word the array will be empty
+      if (letter === key) previousIndex.push(index);
+      return previousIndex;
+    }, ([] as number[]));
+
+  const evaluationInfo: IGuessEvalResult = {
+    indexes,
+    key,
+  };
+
+  return evaluationInfo;
+};
