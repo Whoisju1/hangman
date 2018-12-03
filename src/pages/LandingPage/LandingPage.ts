5b7e76fdbf73fb745064f5d65cbefa742281dd59
Juan John-Charles <whoisju1@gmail.com>
2018-12-03 18:48:00 -0500

    create landing page

--- src/pages/LandingPage/LandingPage.ts
+++ src/pages/LandingPage/LandingPage.ts
@@ -1,16 +1,25 @@
-import { createFragment } from '../../utils/htmlFunctionality/createFragment/createFragment';
+import { ComponentBase } from '../../utils/htmlFunctionality/ComponentBase/componentBase';
+import { createFragment, ICreateFragment } from '../../utils/htmlFunctionality/createFragment/createFragment';
 
-export const LandingPage = createFragment({})`
-  <header class="header">
-    <div class="header__logo">
-        Hangman
+export class LandingPage extends ComponentBase {
+  protected _fragment = createFragment({})`
+    <div class="container">
+      <header class="header">
+        <div class="header__logo">
+          Hangman
+        </div>
+      </header>
+      <h1 class="intro">
+        Press <span class="intro--text">Enter</span> to start game!!!
+      </h1>
     </div>
-  </header>
-  <h1 class="intro">
-    Press <span class="intro--text">Enter</span> to start game!!!
-  </h1>
-`;
+  `;
 
-const introText: HTMLElement = LandingPage.querySelector('.intro--text') as HTMLElement;
+  protected _element = this._fragment.querySelector('*') as HTMLElement;
 
-introText.addEventListener('click', (e) => alert('Start Game!!!'));
+  constructor(public startGame: () => void) {
+    super();
+    const introText = super.fragment.querySelector('.intro--text') as HTMLElement;
+    introText.addEventListener('click', this.startGame);
+  }
+}
