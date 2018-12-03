c2e96db2dfb6b9d034897b7b84e7621ffe91d261
Juan John-Charles <whoisju1@gmail.com>
2018-12-03 18:47:30 -0500

    create base class for creating components

---
+++ src/utils/htmlFunctionality/ComponentBase/componentBase.ts
@@ -0,0 +1,30 @@
+export interface IComponent {
+  fragment: DocumentFragment;
+  removeClasses: (className: string) => void;
+  addClass: (classNames: string | string[]) => void;
+}
+
+export abstract class ComponentBase implements IComponent {
+  protected abstract _fragment: DocumentFragment;
+  protected abstract _element: HTMLElement;
+
+  get fragment() {
+    return this._fragment;
+  }
+
+  public addClass(classNames: string | string[]) {
+    if (Array.isArray(classNames)) {
+      classNames.forEach((className) => this._element.classList.add(className));
+    } else {
+      this._element.classList.add(classNames);
+    }
+  }
+
+  public removeClasses(className: string) {
+    this._element.classList.remove(className);
+  }
+
+  public remove() {
+    this._element.remove();
+  }
+}
